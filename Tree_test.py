df8ffbd13f45e15a11f37d5d1c6ab610c9529a5e
Denis Laliberté <denis@tp1.ca>
2015-01-08 12:51:46 -0500

    remove cnary test

--- Tree_test.py
+++ Tree_test.py
@@ -2,8 +2,6 @@ import unittest
 from Tree import Tree
 
 class Tree_test(unittest.TestCase):
-  def test_canary(self):
-    self.assertEqual(True,True)
   def test_get_node_value(self):
     tree = Tree(1)
     self.assertEqual(tree.getValue(),1)
