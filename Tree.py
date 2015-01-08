fb866fd0eb2dc9846f84d01b6ed996471b71b12d
Denis Laliberté <denis@tp1.ca>
2015-01-08 12:22:53 -0500

    remove hllo wordl

--- Tree.py
+++ Tree.py
@@ -6,8 +6,6 @@ class Tree:
     self.value = value
   def getValue(self):
     return self.value
-  def test(self):
-    return "hello world"
   def add(self,value):
     self.children = Tree(value)
   def search(self,value):
