88221370dfc5cb2fe33c336821e1b76fe070979d
Denis Laliberté <denis@tp1.ca>
2015-01-08 11:52:25 -0500

    add first children

--- Tree.py
+++ Tree.py
@@ -1,9 +1,14 @@
 
 class Tree:
   value = False
+  children = False
   def __init__(self,value):
     self.value = value
   def getValue(self):
     return self.value
   def test(self):
     return "hello world"
+  def add(self,value):
+    self.children = Tree(value)
+  def search(self,value):
+    return self.children
--- Tree_test.py
+++ Tree_test.py
@@ -7,3 +7,7 @@ class Tree_test(unittest.TestCase):
   def test_get_node_value(self):
     tree = Tree(1)
     self.assertEqual(tree.getValue(),1)
+  def test_add_first_child(self):
+    tree = Tree(1)
+    tree.add(2)
+    self.assertEqual(tree.search(2).getValue(),2)
